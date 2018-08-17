12204dbc2d8eafb3ee626741fd715642840ea506
Sam Yong <samyonggit@gmail.com>
2018-08-17 19:06:15 +0100

    add test options

---
+++ src/options/test.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    name: 'verbose',
+    alias: 'V',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Turn on debugging output.',
+    group: 'main'
+  },
+  {
+    name: 'quiet',
+    alias: 'q',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Silence any output, overrides verbose.',
+    group: 'main'
+  },
+  {
+    name: 'program',
+    multiple: true,
+    type: String,
+    defaultOption: true,
+    group: 'hidden'
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Prints this usage guide.',
+    group: 'main'
+  },
+  {
+    name: 'version',
+    alias: 'v',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Print the CLI tools package version. In verbose mode, the version of lps.js is printed.',
+    group: 'main'
+  }
+];
