2be406a4ae6933a7730cdd914a907f80fdc7204c
Sam Yong <samyonggit@gmail.com>
2018-08-17 18:55:36 +0100

    add tracker options definition

---
+++ src/options/tracker.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    name: 'port',
+    alias: 'p',
+    type: Number,
+    defaultValue: 4100,
+    description: 'Set the port number of the observation server. Defaults to 4100. Set to 0 to use any available port.',
+    group: 'main'
+  },
+  {
+    name: 'verbose',
+    alias: 'V',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Turn on debugging output.',
+    group: 'main'
+  },
+  {
+    name: 'quiet',
+    alias: 'q',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Silence any output, overrides verbose.',
+    group: 'main'
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Prints this usage guide.',
+    group: 'main'
+  },
+  {
+    name: 'version',
+    alias: 'v',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Print the CLI tools package version. In verbose mode, the version of lps.js is printed.',
+    group: 'main'
+  }
+];
